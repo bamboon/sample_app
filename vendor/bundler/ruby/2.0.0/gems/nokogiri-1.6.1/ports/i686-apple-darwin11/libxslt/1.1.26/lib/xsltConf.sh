3a36bfa49cf100353bde9bad0bf81ac4e7495236
Takemoto Naoko <bamboonaoko2@gmail.com>
2014-04-17 15:42:04 +0900

    Finish layout and routes

---
+++ vendor/bundler/ruby/2.0.0/gems/nokogiri-1.6.1/ports/i686-apple-darwin11/libxslt/1.1.26/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/saraya/rails_projects/sample_app/vendor/bundler/ruby/2.0.0/gems/nokogiri-1.6.1/ports/i686-apple-darwin11/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/Users/saraya/rails_projects/sample_app/vendor/bundler/ruby/2.0.0/gems/nokogiri-1.6.1/ports/i686-apple-darwin11/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
+XSLT_INCLUDEDIR="-I/Users/saraya/rails_projects/sample_app/vendor/bundler/ruby/2.0.0/gems/nokogiri-1.6.1/ports/i686-apple-darwin11/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
